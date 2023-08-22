2ecbd9d1cd0a8d9ff2fba25712d7ea3ae8c71409
Akanbi Abdullahi <icanbe444@gmail.com>
2023-08-22 21:28:09 +0100

    TableBookingSsystem:Task 1

---
+++ Insert_into_Booking_Table.sql
@@ -0,0 +1,10 @@
+SELECT * FROM LittleLemonDM.Booking;
+
+INSERT INTO Booking (BookingDate, TableNumber, CustomerID)
+VALUES ('2022-10-10', 5 ,1), ('2022-11-12' ,3, 3), ('2022-10-11', 2 ,2), ('2022-10-13', 2 ,1);
+
+	
+
+
+
+
